2a9ad5a21ae83699c959b5245e908fe24356f8dc
Asher Cerka <acerka@springbuk.com>
2022-08-08 14:22:07 -0400

    MOJ-235 Adding create task

--- lib/active_record/tasks/odbc_database_tasks.rb
+++ lib/active_record/tasks/odbc_database_tasks.rb
@@ -7,6 +7,18 @@ module ActiveRecord
         @configuration = configuration
       end
 
+      def create
+        connection.create_database configuration['database']
+        establish_connection configuration
+
+      rescue ActiveRecord::StatementInvalid => error
+        if /Database .* already exists/ === error.message
+          raise DatabaseAlreadyExists
+        else
+          raise
+        end
+      end
+
       ActiveRecord::Tasks::DatabaseTasks.register_task(/odbc/, ActiveRecord::Tasks::ODBCDatabaseTasks)
     end
   end
